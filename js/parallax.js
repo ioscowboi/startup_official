f4d2edccc6b4f6823a41beaeda62c9fb6c7c0229
CharDale Barnes <differentaccounts@hotmail.com>
2015-04-02 18:45:12 -0500

    Added wacky parallax effect just to see what would happen.

--- js/parallax.js
+++ js/parallax.js
@@ -1,7 +1,16 @@
 function parallax(){
  var section = document.getElementById('parallax1');
  
- section.style.top = -(window.pageYOffset / 10)+'px';
+ var section2 = document.getElementById('parallax2');
+ 
+ var section3 = document.getElementById('parallax-transparent');
+ 
+ section.style.top = (window.pageYOffset * 2)+'px';
+ 
+ section2.style.top = -(window.pageYOffset / 6)+'px';
+ 
+ section3.style.top = -(window.pageYOffset / 6)+'px';
+ 
 }
 window.addEventListener("scroll", parallax, false);
 
